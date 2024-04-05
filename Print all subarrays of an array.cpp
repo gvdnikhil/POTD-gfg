12fcd1cb8ffdbbd07ac275eae7e533fb68f79e41
Nikhilgvd <86193313+gvdnikhil@users.noreply.github.com>
2024-04-05 14:49:22 +0530

    Create Print all subarrays of an array.cpp

---
+++ Print all subarrays of an array.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  //  std::cout<<"Hello World";
+    int n = 4;
+   int arr[n] = {1,2,3,4};
+   for (int i=0;i<n;i++)
+   
+   {
+        for ( int j =i;j<n;j++)
+        {
+            for (int k = i;k<=j;k++)
+            {
+                cout<<arr[k];
+            }
+            cout<< endl;
+        }
+   }
+    
+
+    return 0;
+}
